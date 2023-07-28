11bf63318b60a4c625f1fb71fc9dc5ef05d24afa
Ryo Kawabata <kawabata@kinsha.co.jp>
2023-07-28 15:38:28 +0900

    php functionsを削除

--- www/index.php
+++ www/index.php
@@ -2,14 +2,13 @@
 ini_set('display_errors', "On");
 
 include(dirname(__FILE__)."/php/_config.php");
-include(dirname(__FILE__)."/php/_functions.php");
 
 $title = $siteTitle . '';
 $pageDescription = $siteDescription;
 $categoryName = "top";
 $pageName = "";
 
-include(dirname(__FILE__)."/php/_variables.php");
+include("_variables.php");
 $isCommon = false;
 
 ?>
